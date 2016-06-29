c506b980d768a55ca7011ba06ada24a6154dd63c
AnthonJohansson <Anthonjohansson@etech.net>
2016-06-29 08:13:15 +0100

    email notification on server timeout should work. When using app.UseHangfireServer(), include a new 'BackgroundJobServerOptions' object with correct email/Sendgrid settings

---
+++ src/Hangfire.Core/Server/ServerStatusNotifierOptions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangfire.Server
+{
+    public class ServerStatusNotifierOptions
+    {
+
+        public string FromEmail { get; private set; }
+        public string FromName { get; private set; }
+        // we should probably protect these (DAPI)
+        public IList<string> Receivers { get; private set; }
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        
+        public ServerStatusNotifierOptions(string username, string password, string fromEmail, string fromName, IList<String> receivers)
+        {
+            FromEmail = fromEmail;
+            FromName = fromName;           
+            Receivers = receivers;
+            Username = username;
+            Password = password;
+        }
+    }
+}
