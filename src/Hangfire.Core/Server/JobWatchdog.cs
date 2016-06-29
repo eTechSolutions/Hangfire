a9e911fa5b11d1c09bcddf08e28ed1ed810aa683
mahoneyj2 <jamesmahoney@etech.net>
2016-06-29 17:11:17 +0100

    Added JobWatchdog background process - tbc

---
+++ src/Hangfire.Core/Server/JobWatchdog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Hangfire.Server
+{
+    class JobWatchdog : IBackgroundProcess
+    {
+        public static readonly TimeSpan DefaultCheckInterval = TimeSpan.FromMinutes(5);
+
+        private readonly TimeSpan _checkInterval;
+
+        public JobWatchdog(TimeSpan checkInterval)
+        {
+
+            _checkInterval = checkInterval;
+        }
+
+        public void Execute(BackgroundProcessContext context)
+        {
+            // SQL query for the number of failed jobs since last check
+
+            using (var connection = context.Storage.GetConnection())
+            {
+                connection.Heartbeat(context.ServerId);
+            }
+
+            context.Wait(_checkInterval); ;
+        }
+
+        public override string ToString()
+        {
+            return GetType().Name;
+        }
+    }
+}
