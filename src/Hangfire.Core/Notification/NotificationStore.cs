6e01a1320113596abac758601134daed0e48affe
AnthonJohansson <Anthonjohansson@etech.net>
2016-07-14 14:18:38 +0100

    code review change

--- src/Hangfire.Core/Notification/NotificationStore.cs
+++ src/Hangfire.Core/Notification/NotificationStore.cs
@@ -10,6 +10,7 @@ namespace Hangfire.EmailNotification
     {
         private static readonly object LockObject = new object();
         private static NotificationStore _current;
+
         private List<INotifier> _notifiers;
         
         public static NotificationStore Current
